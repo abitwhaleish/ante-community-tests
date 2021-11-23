272650e452737865cfad918193e0938b9b13a349
abitwhaleish <abitwhaleish@ante.finance>
2021-11-23 15:48:09 -0800

    rewrote test using different approach

--- contracts/cream/AnteCREAMUtilizationTest.sol
+++ contracts/cream/AnteCREAMUtilizationTest.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 interface ICToken {
     function getCash() external view returns (uint256);
     function totalBorrows() external view returns (uint256);
+    function totalReserves() external view returns (uint256);
 }
 
 /// @title CREAM utilization rate doesn't spike
@@ -24,24 +25,18 @@ contract AnteCREAMUtilizationTest is AnteTest("CREAM utilization doesn't spike")
         ICToken(0xcE4Fe9b4b8Ff61949DCfeB7e03bc9FAca59D2Eb3)  // crBAL
     ];
 
-    /// @notice minimum period after checkpointing before checkTestPasses call
-    /// is allowed to fail
-    uint32 public constant MIN_PERIOD = 12 hours;
+    // threshold amounts by market
+    // TODO update actual thresholds
+    uint256[5] public thresholds = [
+        50, // crETH  - based on historical rate of XX% between mm/dd/yyyy – mm/dd/yyyy
+        50, // crUSDT - based on historical rate of XX% between mm/dd/yyyy – mm/dd/yyyy
+        50, // crUSDC - based on historical rate of XX% between mm/dd/yyyy – mm/dd/yyyy
+        50, // crCOMP - based on historical rate of XX% between mm/dd/yyyy – mm/dd/yyyy
+        50  // crBAL  - based on historical rate of XX% between mm/dd/yyyy – mm/dd/yyyy
+    ];
 
-    /// @notice minimum interval between allowing subsequent checkpoints
-    /// @dev prevents malicious stakers from preventing a failing test by calling checkpoint() repeatedly
-    uint32 public constant MIN_CHECKPOINT_INTERVAL = 48 hours;
-
-    /// @notice threshold for utilization before high change triggers failure
-    uint256 public constant UTILIZATION_MINIMUM_THRESHOLD = 50; // TODO placeholder, need to calculate
-    
-    /// @notice threshold for utilization rate change
-    uint256 public constant UTILIZATION_CHANGE_THRESHOLD = 12345; // TODO placeholder, need to calculate
-
-    /// @notice last time a checkpoint was taken
-    uint256 public lastCheckpointTime;
-    /// @notice CREAM utilization at last checkpoint
-    uint256 public lastUtilizationRate;
+    // number of tests that can fail before test fails
+    uint32 public constant FAIL_THRESHOLD = 3;
 
     constructor() {
         protocolName = "CREAM Finance";
@@ -50,57 +45,24 @@ contract AnteCREAMUtilizationTest is AnteTest("CREAM utilization doesn't spike")
             ICToken cToken = cTokens[i];
             testedContracts.push(address(cToken));
         }
-        checkpoint();
-    }
-
-    /// @notice take checkpoint of current CREAM utilization
-    function checkpoint() public {
-        require(
-            block.timestamp.sub(lastCheckpointTime) > MIN_CHECKPOINT_INTERVAL,
-            "Cannot call checkpoint more than once every 48 hours"
-        );
-
-        lastCheckpointTime = block.timestamp;
-        lastUtilizationRate = getCurrentUtilization();
     }
 
     /// @notice checks that 
     /// @dev 
     /// @return 
     function checkTestPasses() public view override returns (bool) {
-        uint256 timeSinceLastCheckpoint = block.timestamp.sub(lastCheckpointTime);
-        
-        // if timeSinceLastCheckpoint is less than MIN_PERIOD just return true
-        // don't revert test since this will trigger failure on associated AntePool
-        if (timeSinceLastCheckpoint < MIN_PERIOD) {
-            return true;
-        }
+        uint256 failedMarkets;
 
-        uint256 currUtilization = getCurrentUtilization();
-
-        // if utilization under the min threshold return true 
-        if (currUtilization < UTILIZATION_MINIMUM_THRESHOLD) {
-            return true;
-        }
-
-        // if utilization rate decreased then return true to avoid reversion due to underflow
-        if (lastUtilizationRate >= currUtilization) {
-            return true;
-        }
-
-        return currUtilization.sub(lastUtilizationRate).div(timeSinceLastCheckpoint) < UTILIZATION_CHANGE_THRESHOLD;
-    }
-
-    /// @notice calculate current CREAM utilization
-    function getCurrentUtilization() private view returns (uint256) {
-        uint256 totalCreamBorrows;
-        uint256 totalCreamSupply;
-
+        // do we need to pre-check if getCash + totalBorrows - totalReserves is negative and prevent test reversion?
         for (uint256 i = 0; i < 5; i++) {
             ICToken cToken = cTokens[i];
-            totalCreamBorrows.add(cToken.totalBorrows());
-            totalCreamSupply.add(cToken.getCash());
+            failedMarkets = failedMarkets.add(
+                (cToken.totalBorrows().mul(100).div(
+                cToken.totalBorrows().add(cToken.getCash()).sub(cToken.totalReserves())))
+                > thresholds[i]);
         }
-        return totalCreamBorrows.mul(100).div(totalCreamSupply);
+
+        return failedMarkets >= FAIL_THRESHOLD;
+        
     }
 }
